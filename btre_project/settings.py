7b63364fcc996409e052787723e2fbe7e52ab501
Buddy Siriskaowkul <privatebuddy@gmail.com>
2019-03-28 18:16:34 +0700

    Complete

--- btre_project/settings.py
+++ btre_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig'
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+#Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_HOST_USER = 'test@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS= True
